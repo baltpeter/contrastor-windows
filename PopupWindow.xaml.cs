a8db2ee2fdb01421716a60ed1aa4fef528774a56
Benjamin Altpeter <benjamin@oldpeter.de>
2019-09-21 21:39:13 +0200

    Add global hotkeys for picking the colors
    * Ctrl + Alt + F: Pick the foreground color
    * Ctrl + Alt + B: Pick the background color
    
    They both also show the popup after picking.

--- PopupWindow.xaml.cs
+++ PopupWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Interop;
 
 namespace Contrastor
 {
@@ -11,10 +12,29 @@ namespace Contrastor
     /// </summary>
     public partial class PopupWindow : Window
     {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        enum KeyModifier
+        {
+            None = 0,
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            WinKey = 8
+        }
+
+        protected HwndSource _source;
+        protected IntPtr _handle;
+
         private System.Windows.Forms.NotifyIcon notifyIcon;
         private System.Windows.Forms.ContextMenu contextMenu;
         private System.Windows.Forms.MenuItem aboutMenuItem;
         private System.Windows.Forms.MenuItem showHideMenuItem;
+        private System.Windows.Forms.MenuItem pickFirstMenuItem;
+        private System.Windows.Forms.MenuItem pickSecondMenuItem;
         private System.Windows.Forms.MenuItem quitMenuItem;
 
         public PopupWindow()
@@ -32,14 +52,29 @@ namespace Contrastor
             };
         }
 
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            _handle = new WindowInteropHelper(this).Handle;
+            _source = HwndSource.FromHwnd(_handle);
+            _source.AddHook(HwndHook);
+
+            SetupHotkeys();
+        }
+
         protected override void OnInitialized(EventArgs e)
         {
-            Visibility = Visibility.Hidden;
+            // It seems like we need to show the window once before the hotkeys start working.
+            Show();
+            Hide();
 
             aboutMenuItem = new System.Windows.Forms.MenuItem("A&bout (License)", (_s, _e) => { MessageBox.Show(new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("Contrastor.LICENSE")).ReadToEnd()); });
             showHideMenuItem = new System.Windows.Forms.MenuItem("S&how/Hide", (_s, _e) => { Toggle(); });
+            pickFirstMenuItem = new System.Windows.Forms.MenuItem("Pick f&oreground color (Ctrl + Alt + F)", (_s, _e) => { PickColor(0); Show(); });
+            pickSecondMenuItem = new System.Windows.Forms.MenuItem("Pick b&ackground color (Ctrl + Alt + B)", (_s, _e) => { PickColor(1); Show(); });
             quitMenuItem = new System.Windows.Forms.MenuItem("Q&uit", (_s, _e) => { Application.Current.Shutdown(); });
-            contextMenu = new System.Windows.Forms.ContextMenu(new[] { aboutMenuItem, showHideMenuItem, quitMenuItem });
+            contextMenu = new System.Windows.Forms.ContextMenu(new[] { aboutMenuItem, showHideMenuItem, pickFirstMenuItem, pickSecondMenuItem, quitMenuItem });
 
             // Adapted after: https://stackoverflow.com/a/1475775
             notifyIcon = new System.Windows.Forms.NotifyIcon();
@@ -52,10 +87,34 @@ namespace Contrastor
             base.OnInitialized(e);
         }
 
+        // See: https://www.fluxbytes.com/csharp/how-to-register-a-global-hotkey-for-your-application-in-c/
+        protected void SetupHotkeys()
+        {
+            var res0 = RegisterHotKey(_handle, 0, (int)KeyModifier.Control | (int)KeyModifier.Alt, System.Windows.Forms.Keys.F.GetHashCode());
+            var res1 = RegisterHotKey(_handle, 1, (int)KeyModifier.Control | (int)KeyModifier.Alt, System.Windows.Forms.Keys.B.GetHashCode());
+        }
+
+        protected void RemoveHotkeys()
+        {
+            UnregisterHotKey(_handle, 0);
+            UnregisterHotKey(_handle, 1);
+        }
+
         public void Toggle()
         {
             Visibility = IsVisible ? Visibility.Hidden : Visibility.Visible;
             if (IsVisible) Activate();
+        }
+
+        public void PickColor(int forIndex)
+        {
+            if (forIndex != 0 && forIndex != 1) return;
+
+            var button = forIndex == 0 ? popupControl.firstEyeDropper.Content : popupControl.secondEyeDropper.Content;
+            if (button is System.Windows.Controls.Button)
+            {
+                (button as System.Windows.Controls.Button).RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Primitives.ButtonBase.ClickEvent));
+            }
         }
 
         void notifyIcon_Click(object sender, System.Windows.Forms.MouseEventArgs e)
@@ -69,8 +128,26 @@ namespace Contrastor
             if (e.ChangedButton == MouseButton.Left) this.DragMove();
         }
 
+        IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_HOTKEY = 0x0312;
+
+            if (msg == WM_HOTKEY)
+            {
+                int id = wParam.ToInt32();
+                PickColor(id);
+                Show();
+            }
+
+            return IntPtr.Zero;
+        }
+
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
+            _source.RemoveHook(HwndHook);
+            _source = null;
+            RemoveHotkeys();
+
             notifyIcon.Icon.Dispose();
             notifyIcon.Dispose();
             notifyIcon = null;
